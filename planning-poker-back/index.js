cd2c7b5081e982347138cad29daa7ca5b1b0d489
Jimena Ruiz <jruiz@novatronic.com>
2022-09-30 21:13:49 -0500

    cors

--- planning-poker-back/index.js
+++ planning-poker-back/index.js
@@ -8,6 +8,15 @@ import { socketLogic } from "./socket.js";
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const server = createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" },
@@ -16,7 +25,7 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 8080;
 
 io.on("connection", (socket) => {
- socketLogic(socket)
+  socketLogic(socket);
 });
 
 server.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
