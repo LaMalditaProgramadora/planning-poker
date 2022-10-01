2056e0f94cab4dca82d08609fa08cd674ca594c4
Jimena Ruiz <jruiz@novatronic.com>
2022-09-30 22:14:03 -0500

    cors v.2

--- planning-poker-back/index.js
+++ planning-poker-back/index.js
@@ -11,15 +11,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
